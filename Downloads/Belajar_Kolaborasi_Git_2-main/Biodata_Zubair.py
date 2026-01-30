e060a7d81c9d59b62d99a1cf3345a7dd0a7f298f
Zubair Aqwam Ibadurrohman <zubairaqwamibadurrohman@smkitihsanulfikri.sch.id>
2026-01-30 10:33:33 +0700

    Tambah Biodata

---
+++ Downloads/Belajar_Kolaborasi_Git_2-main/Biodata_Zubair.py
@@ -0,0 +1,24 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Zubair Aqwam"
+hobi = "Membantu mengoptimalkan\n                skill programming"
+alamat = "Jalan Kalisombo No. 18"
+Kota = "Salatiga"
+Propinsi = "Jawa Tengah"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+print ("=======================================")
+ 
+
+
+
